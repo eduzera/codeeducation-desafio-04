80e843fc08babee26b9e5a4be37fed55975504e9
Eduardo Zaghi <eduardo@loldesign.com.br>
2021-04-29 20:35:08 -0300

    feature: add code and githubconfig for workflow

---
+++ src/minus.test.js
@@ -0,0 +1,5 @@
+const minus = require('./minus');
+
+test('minus 3 - 1 to equal 2', () => {
+  expect(minus(3, 1)).toBe(2);
+});
---
+++ src/times.test.js
@@ -0,0 +1,5 @@
+const times = require('./times');
+
+test('multiply 4 * 8 to equal 32', () => {
+  expect(times(4, 8)).toBe(32);
+});
